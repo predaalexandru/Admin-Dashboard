072a390bfbf12e471df555bb6830b0a604499bb5
Preda Alexandru <alexpreda95@yahoo.com>
2021-09-09 11:52:45 +0300

    Initial setup

---
+++ dashboard/src/App.js
@@ -0,0 +1,23 @@
+import {useState} from 'react';
+
+const App = () => {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  }
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  }
+
+
+  return (
+   <div className="container">
+     <h1>React Dashboard</h1>
+   </div>
+  );
+}
+
+export default App;
